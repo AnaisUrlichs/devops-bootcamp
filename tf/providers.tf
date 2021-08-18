0d67024440f2b9dd0f237e2b9c1a94048d0485a6
Alistair Hey <alistair@heyal.co.uk>
2021-08-18 17:37:01 +0100

    Add tf example!
    Signed-off-by: Alistair Hey <alistair@heyal.co.uk>

---
+++ tf/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    civo = {
+      source = "civo/civo"
+      version = "0.10.9"
+    }
+  }
+}
+
+variable "CIVO_TOKEN" {}
+
+provider "civo" {
+  token = var.CIVO_TOKEN
+}
